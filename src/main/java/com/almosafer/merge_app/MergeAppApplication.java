a22c79802c25cadb1d6b8bb8807b874ee65d34e7
laith.alrosan <laith.alrosan@almosafer.com>
2025-02-17 17:37:36 +0300

    Added Merge project structure

---
+++ src/main/java/com/almosafer/merge_app/MergeAppApplication.java
@@ -0,0 +1,13 @@
+package com.almosafer.merge_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almosafer/merge_app/MergeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almosafer.merge_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
